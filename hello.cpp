833aca283670ab128e5f8ce49e382a44c809d518
Abner Castro <abnerfcastro@gmail.com>
2016-07-03 01:29:41 -0300

    Added age support.

--- hello.cpp
+++ hello.cpp
@@ -3,9 +3,15 @@
 int main()
 {
 	std::string name;
+	int age;
+
 	std::cout << "Hello World!" << std::endl;
+	
 	std::cout << "What is your name?" << std::endl;
 	std::cin >> name;
+
+	std::cout << "How old are you?" << std::endl;
+	std::cin >> age;
 
 	std::cout << "Hi, " << name << ". Nice to meet you." << std::endl;
 
