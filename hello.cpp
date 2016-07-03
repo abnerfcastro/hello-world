c3360ed5af9e6bee13a23cc30ccb8af875a46da7
Abner Castro <abnerfcastro@gmail.com>
2016-07-03 00:45:16 -0300

    Added name support.

--- hello.cpp
+++ hello.cpp
@@ -2,6 +2,12 @@
 
 int main()
 {
+	std::string name;
 	std::cout << "Hello World!" << std::endl;
+	std::cout << "What is your name?" << std::endl;
+	std::cin >> name;
+
+	std::cout << "Hi, " << name << ". Nice to meet you." << std::endl;
+
 	return 0;
 }
